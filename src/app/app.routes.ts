1065d79e39ba2ac4aa73e24e79b8ebff5969e771
MostafaSaadatnia <40535745+MostafaSaadatnia@users.noreply.github.com>
2025-09-21 22:29:30 +0330

    feat configure the preloading and rendermode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,34 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'posts' },
+
   {
     path: 'posts',
     loadComponent: () =>
       import('@features/post/pages/posts-list/posts-list').then((m) => m.default),
+    data: { preload: false },
   },
+
   {
     path: 'posts/:slug',
     loadComponent: () =>
       import('@features/post/pages/post-details/post-details').then((m) => m.PostDetailComponent),
+    data: { preload: false },
   },
+
   {
     path: 'editor',
     loadComponent: () =>
       import('@features/post/pages/editor/editor').then((m) => m.PostEditorComponent),
+    data: { preload: false },
   },
+
   {
     path: 'editor/:slug',
     loadComponent: () =>
       import('@features/post/pages/editor/editor').then((m) => m.PostEditorComponent),
+    data: { preload: false },
   },
+
   { path: '**', redirectTo: 'posts' },
 ];
